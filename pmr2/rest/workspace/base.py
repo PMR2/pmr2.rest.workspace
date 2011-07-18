8c80122fa697c2c159e59df262f3ced6eb8aff06
Tommy Yu <tommy.yu@auckland.ac.nz>
2011-07-19 11:04:24 +1200

    Create a base edit view
      - This allows disabling the sending of request origin header, or allow
        it to be defined to something specific.

--- pmr2/rest/workspace/base.py
+++ pmr2/rest/workspace/base.py
@@ -9,10 +9,21 @@ class BaseJsonView(BrowserView):
     """
 
     indent = 2
+    # XXX if we are doing this, we need to make sure this only
+    # return data that is NOT private
+    origin = '*'
 
     def dumps(self, result):
         self.request.response.setHeader('Content-type', 'application/json')
-        # XXX if we are doing this, we need to make sure this only
-        # return data that is NOT private
-        self.request.response.setHeader('Access-Control-Allow-Origin', '*')
+        if self.origin:
+            self.request.response.setHeader('Access-Control-Allow-Origin',
+                self.origin)
         return json.dumps(result, indent=self.indent)
+
+
+class BaseJsonEdit(BaseJsonView):
+    """\
+    Returns list of workspaces within the container.
+    """
+
+    origin = None

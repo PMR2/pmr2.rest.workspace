30e4cf1796317a6a5252ef9c741aef94498ebb1a
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-09-27 15:47:31 +1200

    Remove deprecated imports.

--- pmr2/rest/workspace/tests/test_doctests.py
+++ pmr2/rest/workspace/tests/test_doctests.py
@@ -1,14 +1,7 @@
 import unittest
+import doctest
 
-from zope.testing import doctestunit, doctest
-from zope.component import testing
 from Testing import ZopeTestCase as ztc
-
-from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import PloneSite
-from Products.PloneTestCase.layer import onsetup
-
-from pmr2.testing.base import DocTestCase
 
 from pmr2.app.workspace.tests import base
 
